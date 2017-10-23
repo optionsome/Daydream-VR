393b53f9110478b0a9b5ec334f416f494430a768
Jukkiz <jukki_z@hotmail.com>
2017-10-23 21:31:56 +0300

    Assigments2 ex 1 and 2
    Something something first commit for the exercises

---
+++ Assets/SimpleTeleport.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleTeleport : MonoBehaviour {
+
+    private GlueToHeadset headSetScript;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(GvrControllerInput.ClickButtonDown){
+            GameObject controller = GameObject.Find("CubeController");
+            headSetScript = controller.GetComponent<GlueToHeadset>();
+
+            if(headSetScript.target != null) {
+                this.transform.position = headSetScript.hitPoint + new Vector3(0f, 5f, 0f);
+            }
+        }
+	}
+}
