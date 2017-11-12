11ffa71e3c0bac6b44cfce1d543aa3882e9ccd6c
Jukkiz <jukki_z@hotmail.com>
2017-11-13 01:02:22 +0200

    plates breakable + scoring

---
+++ Assets/rigidBodyScoreScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rigidBodyScoreScript : MonoBehaviour {
+
+    private Rigidbody rb;
+    bool fatalFalling = false;
+    public float velocityTreshold = 15f;
+    public string objectName = "Plate";
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+        fatalFalling = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        float normalizedVelocity = Mathf.Sqrt( rb.velocity.x * rb.velocity.x + rb.velocity.y * rb.velocity.y + rb.velocity.z * rb.velocity.z);
+
+        if(normalizedVelocity > velocityTreshold) {
+            fatalFalling = true;
+        }
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if(fatalFalling) {
+            // Could be used!
+            var explosion = GetComponent<ParticleSystem>();
+            explosion.Play();
+            updateGameScore();
+            Destroy(gameObject, explosion.main.duration);
+        }
+    }
+
+    void updateGameScore() {
+        scoreUiText scoreScript = GameObject.Find("ScoreText").GetComponent<scoreUiText>();
+        scoreScript.updateCurrentScore(100, objectName);
+    }
+}
---
+++ Assets/scoreUiText.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scoreUiText : MonoBehaviour {
+
+    Text txt;
+    private int currentscore = 0;
+
+    float timer = 0f;
+    bool textDisplaying = false;
+    float displayTime = 1f;
+
+    List<Color> coolColors = new List<Color>()
+    {
+        new Color(209, 249, 255),
+        new Color(120, 60, 50),
+        new Color(80, 140, 103)
+    };
+    // Use this for initialization
+    void Start () {
+        txt = gameObject.GetComponent<Text>();
+        textDisplaying = false;
+        timer = 0f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(textDisplaying) {
+            timer += Time.deltaTime;
+
+            if(timer > displayTime) {
+                txt.text = "";
+            }
+        }
+	}
+
+    public void updateCurrentScore(int increase, string brokenObject) {
+
+        txt.color = coolColors[Random.Range(0, coolColors.Count)];
+
+        currentscore += increase;
+        txt.text = "Score " + currentscore + " \n +" + increase;
+        textDisplaying = true;
+        timer = 0f;
+    }
+}
